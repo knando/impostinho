bdc39f4a27115aaadcec4ac840c69ebd743fcb26
Rodrigo <rodri.smedeiross@gmail.com>
2026-08-18 17:28:59 -0300

    Feat: adicionar listar medicos

--- src/main/java/br/com/mesquita/model/Medico.java
+++ src/main/java/br/com/mesquita/model/Medico.java
@@ -38,6 +38,12 @@ public class Medico {
 		this.nome = nome;
 	}
 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return nome;
 	}
---
+++ src/main/java/br/com/mesquita/service/MedicoService.java
@@ -0,0 +1,27 @@
+package br.com.mesquita.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.mesquita.model.Medico;
+import br.com.mesquita.repository.MedicoRepository;
+
+@Service
+public class MedicoService {
+	
+	private MedicoRepository medicoRepository;
+	
+	public MedicoService (MedicoRepository medicoRepository) {
+		this.medicoRepository = medicoRepository;
+	}
+	
+	public List<Medico> listar(){
+		return medicoRepository.findAll();
+	}
+	
+	public Long salvar(Medico medico) {
+		return medicoRepository.save(medico).getId();
+	}
+	
+}
