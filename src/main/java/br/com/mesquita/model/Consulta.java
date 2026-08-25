8747ea10cac40487cf12225dddc49788cb97f47c
Vitor Pinzon Ribeiro <294248@mesquita.com.br>
2026-08-25 17:11:31 -0300

    tarefa tabela consul

--- src/main/java/br/com/mesquita/model/Consulta.java
+++ src/main/java/br/com/mesquita/model/Consulta.java
@@ -1,22 +1,41 @@
 package br.com.mesquita.model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Consulta {
-	private Date horario;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private LocalDateTime horario;
+	
+	@ManyToOne
+	@JoinColumn(name = "medico_id", nullable = false)
 	private Medico medico;
+	
+	@ManyToOne
+	@JoinColumn(name = "paciente_id", nullable = false)
 	private Paciente paciente;
 	
-	public Consulta(Date horario, Medico medico, Paciente paciente) {
+	public Consulta() {
+
+	}
+	public Consulta(LocalDateTime horario, Medico medico, Paciente paciente) {
 		this.horario = horario;
 		this.medico = medico;
 		this.paciente = paciente;
 	}
 	
-	public Date getHorario() {
+	public LocalDateTime getHorario() {
 		return horario;
 	}
-	public void setHorario(Date horario) {
+	public void setHorario(LocalDateTime horario) {
 		this.horario = horario;
 	}
 	public Medico getMedico() {
