4c345c331290536a5fff7ed96d96d1839b427f64
Joao Vitor Ferreira <joaovitorfantunes@gmail.com>
2026-09-01 14:00:13 -0300

    Feat: Lista de médicos demitidos adicionada

--- src/main/java/br/com/mesquita/controller/MedicoController.java
+++ src/main/java/br/com/mesquita/controller/MedicoController.java
@@ -33,12 +33,12 @@ public class MedicoController {
 		return "medico/listar";
 	}
 	
-	@GetMapping("/listar/inativo")
+	@GetMapping("/listar/demitidos")
 	String listarMedicosInativos(Model model) {
 		List<Medico> listaMedico = medicoService.buscaPorStatus(false);
 		model.addAttribute("listaM", listaMedico);
 		model.addAttribute("medico", new Medico());
-		return "medico/listar";
+		return "medico/listarDemitidos";
 	}
 	
 	@GetMapping("/cadastro") 
