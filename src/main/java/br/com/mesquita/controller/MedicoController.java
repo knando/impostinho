fcdc4523e7484a0c115115f1ee6d4229b8c9fc01
Vitor Pinzon Ribeiro <294248@mesquita.com.br>
2026-09-03 16:50:09 -0300

    issue 47

--- src/main/java/br/com/mesquita/controller/MedicoController.java
+++ src/main/java/br/com/mesquita/controller/MedicoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.mesquita.model.Medico;
 import br.com.mesquita.service.MedicoService;
