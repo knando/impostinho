390cfa026addd5eb0b99361afb279b8c5ebf0a27
Joao Vitor Ferreira <joaovitorfantunes@gmail.com>
2026-09-08 17:02:12 -0300

    Função duplicada corrigida

--- src/main/java/br/com/mesquita/model/Paciente.java
+++ src/main/java/br/com/mesquita/model/Paciente.java
@@ -1,9 +1,7 @@
 package br.com.mesquita.model;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
--- src/main/java/br/com/mesquita/service/PacienteService.java
+++ src/main/java/br/com/mesquita/service/PacienteService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import br.com.mesquita.model.Medico;
 import br.com.mesquita.model.Paciente;
 import br.com.mesquita.repository.PacienteRepository;
 
@@ -12,11 +11,6 @@ import br.com.mesquita.repository.PacienteRepository;
 public class PacienteService {
 	
 	private PacienteRepository pacienteRepository;
-	
-	
-	public Paciente buscarPorId(Long id) {
-	    return pacienteRepository.findById(id).orElse(null);
-	}
 	
 	public PacienteService(PacienteRepository pacienteRepository) {
 		this.pacienteRepository = pacienteRepository;
