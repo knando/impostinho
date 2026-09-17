9c7bebdb27ffb0e8ded28d24a2c04ade3528d4eb
Godu-coder <ididwars@gmail.com>
2026-09-17 14:33:31 -0300

    Iss77 (#90)

--- src/main/java/br/com/mesquita/controller/IndexController.java
+++ src/main/java/br/com/mesquita/controller/IndexController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("/")
 public class IndexController {
 
 	@GetMapping
 	public String listarIndex() {
 	
-		return "index";
+		return "home";
 	}
 	
 }
--- src/main/java/br/com/mesquita/controller/PacienteController.java
+++ src/main/java/br/com/mesquita/controller/PacienteController.java
@@ -2,6 +2,7 @@ package br.com.mesquita.controller;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,12 +33,14 @@ public class PacienteController {
 	}
 	
 	@GetMapping("/cadastro")
+	@PreAuthorize("hasAnyRole('ATENDENTE', 'ADMIN')")
 	String cadastrarPacientes(Model model) {
 		model.addAttribute("paciente", new Paciente());
 		return "paciente/cadastro";
 	}
 	
 	@GetMapping("/editar")
+	@PreAuthorize("hasAnyRole('ATENDENTE', 'ADMIN')")
 	public String editarPaciente(@RequestParam("id") Long id, Model model) {
 	    Paciente paciente = pacienteService.buscarPorId(id);
 	    model.addAttribute("paciente", paciente);
